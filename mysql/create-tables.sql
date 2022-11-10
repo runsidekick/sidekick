7b66ae1a10a36c2167119e9c3930ec5579d670f4
Yakup Yasin Kalafat <yasin@thundra.io>
2022-11-10 10:07:08 +0300

    Update create-tables.sql

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -116,11 +116,11 @@ CREATE TABLE ServerStatistics (
 );
 
 CREATE TABLE ProbeTag (
-  id                                    VARCHAR(64)         NOT NULL PRIMARY KEY,
-  workspace_id                    	    VARCHAR(64)         NOT NULL,
-  tag                    			    VARCHAR(64)         NOT NULL,
-  disabled                              BOOLEAN             NOT NULL DEFAULT 0,
-  CONSTRAINT probe_tag UNIQUE (workspace_id, tag)
+    id                                  VARCHAR(64)         NOT NULL PRIMARY KEY,
+    workspace_id                        VARCHAR(64)         NOT NULL,
+    tag                                 VARCHAR(64)         NOT NULL,
+    disabled                            BOOLEAN             NOT NULL DEFAULT 0,
+    CONSTRAINT probe_tag UNIQUE (workspace_id, tag)
 );
 
 CREATE TABLE ApplicationConfig (
